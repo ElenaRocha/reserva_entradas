8e59b3a9704526dbebfb5d79da26a7dfe2b4d34e
Elena Rocha <ele_r.v@hotmail.com>
2023-12-29 21:51:57 +0100

    First commit

---
+++ src/Lógica/conexion.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lógica;
+
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author ele_r
+ */
+public class conexion {
+    public String db = "reserva_entradas";
+    public String url = "jdbc:mysql://127.0.0.1/" + db;
+    public String user = "root";
+    public String pass = "";
+
+    public conexion() {
+    }
+    
+    public Connection conectar(){
+        Connection link = null;
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver.class");
+            link = DriverManager.getConnection(this.url, this.user, this.pass);
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showConfirmDialog(null, e);
+        }
+        
+        return link;
+    }
+}
