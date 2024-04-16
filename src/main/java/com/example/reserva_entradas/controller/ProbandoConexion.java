0d417b79abbe1b9e2670b27e920a221816da7430
Elena Rocha <ele_r.v@hotmail.com>
2024-04-16 23:33:22 +0200

    comprobando conexión a la base de datos

--- src/main/java/com/example/reserva_entradas/controller/ProbandoConexion.java
+++ src/main/java/com/example/reserva_entradas/controller/ProbandoConexion.java
@@ -1,6 +1,27 @@
 package com.example.reserva_entradas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class ProbandoConexion {
-    
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ProbandoConexion(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    };
+
+    @GetMapping("/pruebaConexion")
+    public String pruebaConexionBaseDatos(){
+        try {
+            jdbcTemplate.execute("SELECT 1 FROM DUAL");
+            return "La conexión fue correcta";
+        } catch (Exception e) {
+            return "Error en la conexión" + e.getMessage();
+        }
+    }
+
 }
