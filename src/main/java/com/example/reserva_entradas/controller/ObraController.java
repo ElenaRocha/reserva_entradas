95ed9101635f391446019ddd2ce98d76268048a3
Elena Rocha <ele_r.v@hotmail.com>
2024-04-29 22:32:00 +0200

    mapeo

--- src/main/java/com/example/reserva_entradas/controller/ObraController.java
+++ src/main/java/com/example/reserva_entradas/controller/ObraController.java
@@ -1,5 +1,10 @@
 package com.example.reserva_entradas.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cartelera/obras")
 public class ObraController {
     
 }
