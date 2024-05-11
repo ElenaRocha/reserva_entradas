4f736bb6e59444e5d62b76c1134dc8f6d113c252
Elena Rocha <ele_r.v@hotmail.com>
2024-05-11 22:00:14 +0200

    js para recoger la información a modificar

---
+++ src/main/resources/static/obrasActualizar.js
@@ -0,0 +1,14 @@
+function seleccionarModificar(){
+
+    var table = document.getElementById("tabla_obra");
+
+    for(var i = 1; i < table.ariaRowSpan.length; i++){
+        table.rows[i].onclick = function(){
+            document.getElementById("idobra").value = this.cells[0].innerHTML;
+            document.getElementById("titulo").value = this.cells[0].innerHTML;
+            document.getElementById("autor").value = this.cells[0].innerHTML;
+            document.getElementById("duracion_min").value = this.cells[0].innerHTML;
+            document.getElementById("teatro").value = this.cells[0].innerHTML;
+        }
+    }
+}
